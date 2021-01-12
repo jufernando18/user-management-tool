d8e297a00744c35365ad602f470ef5030ac1bf11
Juan Fernando Echavarria Zapata <juanfechavarria@iesonline.com.co>
2021-01-11 19:39:11 -0500

    feat: Dashboard: Se consume y listan los usuarios del sistema

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,23 +1,40 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 export default {
   name: 'dashboard',
   components: {},
   props: {
-    msg: String
+    msg: String,
   },
   data () {
     return {
-
+      users: null
     }
   },
-  computed: {
-
+  created: function () {
+    this.getUsers(); 
   },
   mounted () {
-
   },
   methods: {
-
+    getUsers() {
+      Vue.use(VueAxios, axios);
+      axios.get(
+        'https://jsonplaceholder.typicode.com/users', 
+        { headers: {
+          'content-type': 'application/json'
+          }
+        }
+      ).then((response) => {
+        console.log(response.data);
+        this.users = response.data;
+      }).catch((error) => {
+        console.log(error);
+        this.users = [];
+      });
+    }
   }
 }
 
