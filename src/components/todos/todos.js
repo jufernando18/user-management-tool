4a01a4b73de854ae6f6343666b6695cf1db427cf
Juan Fernando Echavarria Zapata <juanfechavarria@iesonline.com.co>
2021-01-11 23:30:24 -0500

    feat: Dashboard|Todos: Se modifica url de todos de acuerdo a los requerimientos. Se lee el id del todo, se consulta y mapea correctamente la información de los todos en la tabla.

--- src/components/todos/todos.js
+++ src/components/todos/todos.js
@@ -1,3 +1,6 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 export default {
   name: 'todos',
@@ -5,17 +8,31 @@ export default {
   props: [],
   data () {
     return {
-
+      todos: null
     }
   },
   computed: {
-
   },
   mounted () {
-
+    this.getTodos();
   },
   methods: {
-
+    getTodos() {
+      Vue.use(VueAxios, axios);
+      axios.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + this.$route.params.id, 
+        { headers: {
+          'content-type': 'application/json'
+          }
+        }
+      ).then((response) => {
+        console.log(response.data);
+        this.todos = response.data;
+      }).catch((error) => {
+        console.log(error);
+        this.users = [];
+      });
+    }
   }
 }
 
